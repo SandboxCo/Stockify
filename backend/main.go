4f6bd497fc41184bac757f7621661e01352196e2
Omer Adeel <oadeel@uwaterloo.ca>
2023-12-13 22:53:28 -0500

    added api

--- backend/main.go
+++ backend/main.go
@@ -3,18 +3,34 @@ package main
 import (
 	"context"
 	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"os"
 
 	"github.com/SandboxCo/Humanity360/backend/application"
-	"github.com/SandboxCo/Humanity360/backend/datapipeline"
 )
 
 func main() {
 	app := application.New()
 	//datapipeline.StartProducing()
-	datapipeline.StartConsuming()
+	//datapipeline.StartConsuming()
 
 	err := app.Start(context.TODO())
 	if err != nil {
 		fmt.Println("failed to start app", err)
 	}
+
+	response, err := http.Get("http://site.api.espn.com/apis/site/v2/sports/basketball/nba/news")
+
+	if err != nil {
+		fmt.Print(err.Error())
+		os.Exit(1)
+	}
+
+	responseData, err := ioutil.ReadAll(response.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(string(responseData))
 }
