6ced1f2e07ac7973c606091619e6e135c0fa0a06
Omer Adeel <oadeel@uwaterloo.ca>
2024-01-02 01:31:08 -0500

    finished kafka pipeline

--- backend/scraping_service/go.mod
+++ backend/scraping_service/go.mod
@@ -5,20 +5,29 @@ go 1.21.3
 require github.com/gocolly/colly/v2 v2.1.0
 
 require (
+	github.com/0xAX/notificator v0.0.0-20220220101646-ee9b8921e557 // indirect
 	github.com/PuerkitoBio/goquery v1.8.1 // indirect
 	github.com/andybalholm/cascadia v1.3.2 // indirect
 	github.com/antchfx/htmlquery v1.3.0 // indirect
 	github.com/antchfx/xmlquery v1.3.18 // indirect
 	github.com/antchfx/xpath v1.2.5 // indirect
+	github.com/codegangsta/envy v0.0.0-20141216192214-4b78388c8ce4 // indirect
+	github.com/codegangsta/gin v0.0.0-20230218063734-2c98d96c9244 // indirect
+	github.com/confluentinc/confluent-kafka-go v1.9.2 // indirect
 	github.com/confluentinc/confluent-kafka-go/v2 v2.3.0 // indirect
+	github.com/cpuguy83/go-md2man/v2 v2.0.2 // indirect
 	github.com/gobwas/glob v0.2.3 // indirect
 	github.com/golang/groupcache v0.0.0-20210331224755-41bb18bfe9da // indirect
 	github.com/golang/protobuf v1.5.3 // indirect
 	github.com/kennygrant/sanitize v1.2.4 // indirect
+	github.com/mattn/go-shellwords v1.0.12 // indirect
+	github.com/russross/blackfriday/v2 v2.1.0 // indirect
 	github.com/saintfish/chardet v0.0.0-20230101081208-5e3ef4b5456d // indirect
 	github.com/temoto/robotstxt v1.1.2 // indirect
+	github.com/urfave/cli v1.22.14 // indirect
 	golang.org/x/net v0.19.0 // indirect
 	golang.org/x/text v0.14.0 // indirect
 	google.golang.org/appengine v1.6.8 // indirect
 	google.golang.org/protobuf v1.32.0 // indirect
+	gopkg.in/confluentinc/confluent-kafka-go.v1 v1.8.2 // indirect
 )
--- backend/scraping_service/main.go
+++ backend/scraping_service/main.go
@@ -1,22 +1,28 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
 	"strconv"
-	"time"
 
-	"github.com/confluentinc/confluent-kafka-go/kafka"
+	"github.com/confluentinc/confluent-kafka-go/v2/kafka"
 	"github.com/gocolly/colly/v2"
 )
 
 type Article struct {
-	header    string
-	sub_title string
-	url       string
-	date      string
-	source    string
-	img_url   string
+	Header   string `json:"header"`
+	Subtitle string `json:"sub_title"`
+	Url      string `json:"url"`
+	Date     string `json:"date"`
+	Source   string `json:"source"`
+	Imgurl   string `json:"img_url"`
+}
+
+// MessageObject represents the structure of the JSON message
+type MessageObject struct {
+	ID   int    `json:"id"`
+	Data string `json:"data"`
 }
 
 func main() {
@@ -30,54 +36,35 @@ func main() {
 
 	c.OnHTML(".js-stream-content", func(element *colly.HTMLElement) {
 		article := Article{
-			header:    "",
-			sub_title: "",
-			url:       "",
-			date:      "",
-			source:    "Yahoo Finance",
+			Header:   "",
+			Subtitle: "",
+			Url:      "",
+			Date:     "",
+			Source:   "Yahoo Finance",
+			Imgurl:   "",
 		}
 
 		element.ForEach("a", func(_ int, child *colly.HTMLElement) {
-			article.header = child.Text
-			article.url = child.Attr("href")
+			article.Header = child.Text
+			article.Url = child.Attr("href")
 		})
 
 		element.ForEach("p", func(_ int, child *colly.HTMLElement) {
-			article.sub_title = child.Text
+			article.Subtitle = child.Text
 		})
 
 		element.ForEach("img", func(_ int, child *colly.HTMLElement) {
-			article.img_url = child.Attr("src")
+			article.Imgurl = child.Attr("src")
 		})
 
 		parsedArticles = append(parsedArticles, article)
 	})
 
-	// Visit the URL and start scraping
+	//Visit the URL and start scraping
 	err := c.Visit(url)
 	if err != nil {
 		fmt.Println("error occured")
 		log.Fatal(err)
-	}
-
-	p, err := kafka.NewProducer(&kafka.ConfigMap{"bootstrap.servers": "localhost:9092"})
-	if err != nil {
-		panic(err)
-	}
-
-	defer p.Close()
-
-	topic := "myTopic"
-	for counter := 0; ; counter++ {
-		value := "Message #" + strconv.Itoa(counter)
-		p.Produce(&kafka.Message{
-			TopicPartition: kafka.TopicPartition{Topic: &topic, Partition: kafka.PartitionAny},
-			Value:          []byte(value),
-		}, nil)
-
-		fmt.Println("Sent:", value)
-		p.Flush(15 * 1000)
-		time.Sleep(1 * time.Second)
 	}
 
 	// url = "https://www.cnbc.com/personal-finance/"
@@ -110,26 +97,26 @@ func main() {
 	// 		fmt.Println(child)
 	// 	})
 
-		// count := 1
-		// element.ForEach("a", func(_ int, child *colly.HTMLElement) {
-		// 	if count == 1 {
-		// 		article.header = child.Text
-		// 		article.url = child.Attr("href")
-		// 		count = count + 1
-		// 		fmt.Println(child.Text, child.Attr("href"))
-		// 	}
-		// })
+	// count := 1
+	// element.ForEach("a", func(_ int, child *colly.HTMLElement) {
+	// 	if count == 1 {
+	// 		article.header = child.Text
+	// 		article.url = child.Attr("href")
+	// 		count = count + 1
+	// 		fmt.Println(child.Text, child.Attr("href"))
+	// 	}
+	// })
 
-		// element.ForEach("p", func(_ int, child *colly.HTMLElement) {
-		// 	article.sub_title = child.Text
-		// })
+	// element.ForEach("p", func(_ int, child *colly.HTMLElement) {
+	// 	article.sub_title = child.Text
+	// })
 
-		// element.ForEach("img", func(_ int, child *colly.HTMLElement) {
-		// 	article.img_url = child.Attr("src")
-		// })
+	// element.ForEach("img", func(_ int, child *colly.HTMLElement) {
+	// 	article.img_url = child.Attr("src")
+	// })
 
-		parsedArticles = append(parsedArticles, article)
-	})
+	//parsedArticles = append(parsedArticles, article)
+	//	})
 
 	// Visit the URL and start scraping
 	// err = c.Visit(url)
@@ -138,8 +125,59 @@ func main() {
 	// 	log.Fatal(err)
 	// }
 
-	// Access the parsed data after scraping
-	// for _, element := range parsedArticles {
-	// 	fmt.Printf("%+v\n", element)
-	// }
+	p, err := kafka.NewProducer(&kafka.ConfigMap{
+		"bootstrap.servers": "pkc-921jm.us-east-2.aws.confluent.cloud:9092",
+		"security.protocol": "SASL_SSL",
+		"sasl.mechanisms":   "PLAIN",
+		"sasl.username":     "N5Z7Y6TAOZSDS4HZ",
+		"sasl.password":     "4u1M5DvvpnsBxryH9OI5zKa7kTrkDvKojIjvtjl/1WaB+6OYwgX/XehLzW9v1ZeV",
+		// Add other configuration properties as needed
+	})
+
+	if err != nil {
+		panic(err)
+	}
+
+	defer p.Close()
+
+	//Topic to produce messages to
+	topic := "topic_0"
+
+	//Access the parsed data after scraping
+	for i, element := range parsedArticles {
+
+		fmt.Printf("%+v\n", element)
+
+		messageValue, err := json.Marshal(element)
+
+		// Convert int to string
+		key := strconv.Itoa(i)
+
+		// Produce a message to the topic
+		message := &kafka.Message{
+			TopicPartition: kafka.TopicPartition{Topic: &topic, Partition: kafka.PartitionAny},
+			Key:            []byte(key),
+			Value:          messageValue,
+		}
+
+		// Produce the message and j handle errors
+		err = p.Produce(message, nil)
+		if err != nil {
+			fmt.Printf("Failed to produce message: %s\n", err)
+			return
+		}
+
+		// Wait for the message to be delivered to the broker
+		deliveryReport := <-p.Events()
+		switch e := deliveryReport.(type) {
+		case *kafka.Message:
+			if e.TopicPartition.Error != nil {
+				fmt.Printf("Delivery failed: %v\n", e.TopicPartition.Error)
+			} else {
+				fmt.Printf("Delivered message to topic %s [%d] at offset %v\n",
+					*e.TopicPartition.Topic, e.TopicPartition.Partition, e.TopicPartition.Offset)
+			}
+		}
+	}
+
 }
