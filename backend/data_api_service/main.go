7fb879ec22cd1647520c4fe85b23c59d5f20705b
Omer Adeel <oadeel@uwaterloo.ca>
2024-01-06 01:43:52 -0500

    finished changes

--- backend/data_api_service/main.go
+++ backend/data_api_service/main.go
@@ -3,21 +3,23 @@ package main
 import (
 	"encoding/json"
 	"fmt"
-	"io"
 	"log"
-	"net/http"
 	"strconv"
+	"time"
 
 	"github.com/confluentinc/confluent-kafka-go/v2/kafka"
+	"github.com/gocolly/colly/v2"
 )
 
 // Stock represents a stock entity
 type Stock struct {
-	Ticker   string `json:"ticker"`
-	Name     string `json:"name"`
-	Type     string `json:"type"`
-	Currency string `json:"currency_name"`
-	Date     string `json:"last_updated_utc"`
+	Ticker        string    `json:"ticker"`
+	Name          string    `json:"name"`
+	Portfolio     string    `json:"portfolio"`
+	Price         string    `json:"price"`
+	Change        string    `json:change`
+	Percentchange string    `json:"percentchange"`
+	Date          time.Time `json:"last_updated_utc"`
 }
 
 // StockResponse represents the JSON response structure
@@ -26,37 +28,85 @@ type StockResponse struct {
 }
 
 func main() {
-	// Make an HTTP GET request to the Polygon API
-	stockResponse, err := http.Get("https://api.polygon.io/v3/reference/tickers?active=true&apiKey=QPFs6luFf15cS9kaDOcGp9GwhzGh0482")
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer stockResponse.Body.Close()
+	// Create a new collector
+	c := colly.NewCollector()
 
-	// Read the response body
-	body, err := io.ReadAll(stockResponse.Body)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	// Unmarshal JSON into struct
-	var stockData StockResponse
-	err = json.Unmarshal(body, &stockData)
-	if err != nil {
-		log.Fatal(err)
-	}
+	// Define the URL you want to scrape
+	url := "https://www.slickcharts.com/sp500"
 
 	var parsedStocks []Stock
-	for _, stock := range stockData.Results {
-		stockAdd := Stock{
-			Ticker:   stock.Ticker,
-			Name:     stock.Name,
-			Type:     stock.Type,
-			Currency: stock.Currency,
-			Date:     stock.Date,
+
+	c.OnHTML(".col-lg-7", func(element *colly.HTMLElement) {
+		stock := Stock{
+			Ticker:        "",
+			Name:          "",
+			Portfolio:     "",
+			Price:         "",
+			Change:        "",
+			Percentchange: "",
+			Date:          time.Now(),
 		}
-		parsedStocks = append(parsedStocks, stockAdd)
+
+		element.ForEach("tr", func(_ int, child *colly.HTMLElement) {
+			count := 0
+			child.ForEach("td", func(_ int, child *colly.HTMLElement) {
+				if count == 1 {
+					stock.Name = child.ChildText("a:first-child")
+				} else if count == 2 {
+					stock.Ticker = child.ChildText("a:first-child")
+				} else if count == 3 {
+					stock.Portfolio = child.Text
+				} else if count == 4 {
+					stock.Price = child.Text
+				} else if count == 5 {
+					stock.Change = child.Text
+				} else if count == 6 {
+					stock.Percentchange = child.Text
+				}
+				count = count + 1
+			})
+			parsedStocks = append(parsedStocks, stock)
+			//fmt.Printf("Stock Object: %v\n", stock)
+		})
+	})
+
+	//Visit the URL and start scraping
+	err := c.Visit(url)
+	if err != nil {
+		fmt.Println("error occured")
+		log.Fatal(err)
 	}
+
+	// stockResponse, err := http.Get("https://api.polygon.io/v3/reference/tickers?active=true&apiKey=QPFs6luFf15cS9kaDOcGp9GwhzGh0482")
+	// if err != nil {
+	// 	log.Fatal(err)
+	// }
+	// defer stockResponse.Body.Close()
+
+	// // Read the response body
+	// body, err := io.ReadAll(stockResponse.Body)
+	// if err != nil {
+	// 	log.Fatal(err)
+	// }
+
+	// // Unmarshal JSON into struct
+	// var stockData StockResponse
+	// err = json.Unmarshal(body, &stockData)
+	// if err != nil {
+	// 	log.Fatal(err)
+	// }
+
+	// var parsedStocks []Stock
+	// for _, stock := range stockData.Results {
+	// 	stockAdd := Stock{
+	// 		Ticker:   stock.Ticker,
+	// 		Name:     stock.Name,
+	// 		Type:     stock.Type,
+	// 		Currency: stock.Currency,
+	// 		Date:     stock.Date,
+	// 	}
+	// 	parsedStocks = append(parsedStocks, stockAdd)
+	// }
 
 	p, err := kafka.NewProducer(&kafka.ConfigMap{
 		"bootstrap.servers": "pkc-921jm.us-east-2.aws.confluent.cloud:9092",
@@ -112,5 +162,4 @@ func main() {
 			}
 		}
 	}
-
 }
